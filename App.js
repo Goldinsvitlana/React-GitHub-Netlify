d6ad5c6448a63ffdb50c1b0918716af413f52110
Svitlana <113310737+Goldinsvitlana@users.noreply.github.com>
2023-03-10 22:12:52 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './index.css';
+import Weather from './Weather';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Weather App</h1>
+        <Weather />
+      </header>
+    </div>
+  );
+}
+
+export default App;
